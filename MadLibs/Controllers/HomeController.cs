47550246e783b3670e8ce4c3e1da43f46cf9787e
Tiberius Lockett <tlockett680@gmail.com>
2022-09-26 13:19:28 -0700

    Add Grocery and Puppy madlibs
    Co-Authored-By: Noah Lundquist <nalundquist@gmail.com>
    co-authored-by: Shaniza Lingle <shanizalingle@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -8,10 +8,44 @@ namespace MadLibs.Controllers
     [Route ("/")]
     public ActionResult Home() {return View();}
 
-    [Route ("/result")]
-    public ActionResult Result(string noun, string verb, string adj)
+    [Route ("/puppy-story")]
+    public ActionResult PuppyForm() {return View();}
+
+    [Route ("/grocery-trip")]
+    public ActionResult GroceryForm() {return View();}
+
+    [Route ("/circus-day")]
+    public ActionResult CircusForm() {return View();}
+
+
+
+    [Route ("/puppy-story2")]
+    public ActionResult PuppyResult(string food1, string food2, string food3, string noun, string verb, string adj)
     {
-      
+      Words userWords = new Words();
+      userWords.FoodName1 = food1;
+      userWords.FoodName2 = food2;
+      userWords.FoodName3 = food3;
+      userWords.Noun = noun;
+      userWords.Verb = verb;
+      userWords.Adj = adj;
+
+      return View(userWords);
+    }
+
+    [Route ("/grocery-story2")]
+    public ActionResult GroceryResult(string grandmaName, string food1, string food2, string food3, string noun, string verb, string adj)
+    {
+      Words userWords = new Words();
+      userWords.GrandmaName = grandmaName;
+      userWords.FoodName1 = food1;
+      userWords.FoodName2 = food2;
+      userWords.FoodName3 = food3;
+      userWords.Noun = noun;
+      userWords.Verb = verb;
+      userWords.Adj = adj;
+
+      return View(userWords);
     }
   }
 }
--- MadLibs/Models/Words.cs
+++ MadLibs/Models/Words.cs
@@ -4,6 +4,10 @@ namespace MadLibs.Models
 {
   public class Words
   {
+    public string GrandmaName { get; set; }
+    public string FoodName1 { get; set; }
+    public string FoodName2 { get; set; }
+    public string FoodName3 { get; set; }
     public string Noun { get; set;}
     public string Verb { get; set;}
     public string Adj { get; set;}
